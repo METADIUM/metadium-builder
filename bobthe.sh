9435f0e452bc584924f816a1dc747a68880be114
Uh Sado <sadoc@metadium.com>
2019-04-01 21:03:49 +0000

    added password option to governance initialization

--- bobthe.sh
+++ bobthe.sh
@@ -234,7 +234,7 @@ function setup_cluster ()
 
     # initialize governance
     echo "initializing governance"
-    docker exec -it -u ${USERID} ${node_prefix}${node_index} /opt/meta/bin/gmet.sh init-gov meta /opt/meta/config.json /opt/meta/keystore/account-01 || die "Governance initialization failed"
+    docker exec -it -u ${USERID} ${node_prefix}${node_index} /opt/meta/bin/gmet attach http://localhost:8588 --preload "/opt/meta/conf/MetadiumGovernance.js,/opt/meta/conf/deploy-governance.js" --exec 'GovernanceDeployer.deploy("/opt/meta/keystore/account-01", "password", "/opt/meta/config.json")' || die "Governance initialization failed"
 
     # run admin.etcdInit() if governance is initialized
     echo -n "initializing etcd..."
